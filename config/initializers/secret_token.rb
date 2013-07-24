0c6846ab7347bc5a8501c6f73a39404eae4c8f99
Guilherme Teodoro <gui.teo2@gmail.com>
2013-07-23 23:06:13 -0300

    Rails project started (the real thing)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apontador::Application.config.secret_token = '7e035f22b94e57fff41d25ece20911acf3a4d5315370b1a3676883273bf97e6574be3687967326ddfeba7ea8ecccbecae7b9ed20b148996f3fd0e01a24c8dcf0'
