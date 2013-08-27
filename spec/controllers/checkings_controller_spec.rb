c8771cd149d7d99c0e36eb341bc5ff965cd6892e
Guilherme <gui.teo2@gmail.com>
2013-08-27 18:01:21 -0300

    começando Checkings_Controller

--- spec/controllers/checkings_controller_spec.rb
+++ spec/controllers/checkings_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe CheckingsController do
+  before do
+    @manager = FactoryGirl.create(:user, manager: true)
+    @employee = FactoryGirl.create(:user, manager: false)
+  end
 
+  describe "GET #new" do
+    it "if logged and manager" do
+      get :new, {}, {id: @manager.id, name: @manager.name, manager: true}
+      response.should be_successful
+    end
+    it "redirects if not manager" do
+      get :new, {}, {id: @employee.id, name: @employee.name, manager: @employee.manager?}
+      response.should redirect_to login_path
+    end
+  end
 end
