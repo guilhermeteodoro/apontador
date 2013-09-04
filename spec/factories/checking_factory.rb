dfc4873e66f4733d9b1569c44b4bed7c02b5477c
Eustaquio Rangel <taq@eustaquiorangel.com>
2013-09-04 14:52:25 -0300

    Randomizei os minutos também.
    Fica melhor criar dados mais aleatórios para ver o retorno dos testes. Do jeito
    que estava, os minutos sempre ficavam em 0.

--- spec/factories/checking_factory.rb
+++ spec/factories/checking_factory.rb
@@ -1,14 +1,10 @@
 FactoryGirl.define do
-
   factory :checking, class: Checking do
-
     checked_in_at   { Time.now }
-    checked_out_at  { Time.now + rand(1..6).hours }
+    checked_out_at  { Time.now + rand(1..6).hours + rand(1..59).minutes }
     approved        { [true, false].sample }
     paid            { [true, false].sample }
     hour_value      { rand(10.0..70.0) }
     user_id         { rand(0..1) }
-
   end
-
 end
