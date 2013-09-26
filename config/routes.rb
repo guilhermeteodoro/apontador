67885dbe8382980fa4d88f5f00d216caf1b4d223
Guilherme <gui.teo2@gmail.com>
2013-09-26 17:50:34 -0300

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,66 +1,66 @@
 Checker::Application.routes.draw do
 
   controller :users do
-    match 'manager/employee/new'            => 'users#new', via: :get, as: 'new_user'
-    match 'manager/employee'                => 'users#create', via: :post, as: 'users'
-    match 'manager/employee/:username'      => 'users#show', via: :get, as: 'user'
-    match 'manager/employee/:username'      => 'users#update', via: :put, as: 'user'
-    match 'manager/employee/:username'      => 'users#destroy', via: :delete, as: 'user'
-    match 'manager/employee/:username/edit' => 'users#edit', via: :get, as: 'edit_user'
+    get     'manager/employee/new'            => 'users#new', as: 'new_user'
+    post    'manager/employee'                => 'users#create', as: 'users'
+    get     'manager/employee/:username'      => 'users#show', as: 'user'
+    put     'manager/employee/:username'      => 'users#update', as: 'user'
+    delete  'manager/employee/:username'      => 'users#destroy', as: 'user'
+    get     'manager/employee/:username/edit' => 'users#edit', as: 'edit_user'
   end
 
   controller "manager::users" do
-    match 'manager'       => 'manager/users#index', via: :get, as: 'manager_users'
-    match 'signup'        => 'manager/users#new', as: 'signup'
-    match 'manager'       => 'manager/users#create', via: :post, as: 'manager_users'
-    match 'manager'       => 'manager/users#update', via: :put, as: 'manager_user'
-    match 'manager'       => 'manager/users#destroy', via: :delete, as: 'manager_user'
-    match 'manager/edit'  => 'manager/users#edit', via: :get, as: 'edit_manager_user'
+    get     'manager'       => 'manager/users#index', as: 'manager_users'
+    get     'signup'        => 'manager/users#new', as: 'signup'
+    post    'manager'       => 'manager/users#create', as: 'manager_users'
+    put     'manager'       => 'manager/users#update', as: 'manager_user'
+    delete  'manager'       => 'manager/users#destroy', as: 'manager_user'
+    get     'manager/edit'  => 'manager/users#edit', as: 'edit_manager_user'
   end
 
   controller :checkings do
-    match 'check-in'    => 'checkings#create', via: :post, as: 'checkings'
-    match 'check-in'    => 'checkings#new', via: :get, as: 'check_in'
-    match 'check-out'   => 'checkings#edit', via: :get, as: 'check_out'
-    match 'check-out'   => 'checkings#update', via: :put, as: 'checking'
+    post  'check-in'    => 'checkings#create', as: 'checkings'
+    get   'check-in'    => 'checkings#new', as: 'check_in'
+    get   'check-out'   => 'checkings#edit', as: 'check_out'
+    put   'check-out'   => 'checkings#update', as: 'checking'
   end
 
   controller :users do
-    match 'edit'    => 'users#edit', via: :get, as: 'edit_employee_user'
-    match 'edit'    => 'users#update', via: :put, as: 'edit_employee_user'
-    match 'report'  => 'users#report', via: :get, as: 'report_employee'
+    get 'edit'    => 'users#edit', as: 'edit_employee_user'
+    put 'edit'    => 'users#update', as: 'edit_employee_user'
+    get 'report'  => 'users#report', as: 'report_employee'
   end
 
   controller :companiers do
-    match 'manager/company/new' => 'companies#new', as: 'new_company'
-    match 'manager/company'     => 'companies#create', via: :post, as: 'company'
-    match 'manager/company'     => 'companies#update', via: :put, as: 'company'
-    match 'manager/company'     => 'companies#destroy', via: :delete, as: 'company'
+    get     'manager/company/new' => 'companies#new', as: 'new_company'
+    post    'manager/company'     => 'companies#create', as: 'company'
+    put     'manager/company'     => 'companies#update', as: 'company'
+    delete  'manager/company'     => 'companies#destroy', as: 'company'
   end
 
   controller :tasks do
-    match 'no-task'             => 'tasks#no_task', as: 'no_task'
-    match 'task-in-negotiation' => 'tasks#task_in_negotiation', as: 'task_in_negotiation'
+    get 'no-task'             => 'tasks#no_task', as: 'no_task'
+    get 'task-in-negotiation' => 'tasks#task_in_negotiation', as: 'task_in_negotiation'
   end
 
   controller :tasks do
-    match 'manager/task/new'      => 'tasks#new', via: :get, as: 'new_task'
-    match 'manager/task'          => 'tasks#create', via: :post, as: 'task'
-    # match 'manager/task/:id'      => 'tasks#index', via: :get, as: 'task'
-    match 'manager/task/:id'      => 'tasks#update', via: :put, as: 'task'
-    match 'manager/task/edit/:id' => 'tasks#edit', as: 'edit_task'
+    get 'manager/task/:id'      => 'tasks#show', as: 'task'
+    put 'manager/task/:id'      => 'tasks#update', as: 'task'
+    get 'manager/task/new'      => 'tasks#new', as: 'new_task'
+    post 'manager/task'         => 'tasks#create', as: 'task'
+    # get 'manager/tasks'       => 'tasks#index', as: 'task'
+    get 'manager/task/edit/:id' => 'tasks#edit', as: 'edit_task'
   end
 
   controller :sessions do
     match 'login'  => 'sessions#login', as: 'login'
-    match 'logout' => 'sessions#logout', as: 'logout'
+    get   'logout' => 'sessions#logout', as: 'logout'
   end
 
   controller :public do
-    match 'index' => 'public#index'
+    get 'index' => 'public#index'
   end
 
-  match '/' => 'public#redirecter'
   root to: 'public#redirecter'
 
 end
